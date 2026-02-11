805fbaa2f7b5738400e892dae65dedd76f8e5f8f
vlyssidisp-design <vlyssidisp@gmail.com>
2026-02-11 06:03:49 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Receipts Manager</h1>
+      <p>Η εφαρμογή αποθήκευσης αποδείξεων λειτουργεί ✅</p>
+    </div>
+  );
+}
+
+export default App;
